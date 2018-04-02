c293631768911e968336b44f2a41a41c48cc676a
Gabi Kazav <gabik@google.com>
2018-04-02 09:51:13 +0300

    rm vis

--- ta/sentdex.py
+++ ta/sentdex.py
@@ -3,8 +3,8 @@
 import bs4 as bs
 from statistics import mean
 import datetime as dt
-import matplotlib.pyplot as plt
-from matplotlib import style
+#import matplotlib.pyplot as plt
+#from matplotlib import style
 import numpy as np
 import os
 import pandas as pd
@@ -17,7 +17,7 @@ from sklearn.ensemble import VotingClassifier, RandomForestClassifier
 import warnings
 warnings.simplefilter('ignore')
 
-style.use('ggplot')
+#style.use('ggplot')
 
 def save_sp500_tickers():
     resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
@@ -84,30 +84,30 @@ def compile_data():
     main_df.to_csv('sp500_joined_closes.csv')
 
 
-def visualize_data():
-    df = pd.read_csv('sp500_joined_closes.csv')
-    df_corr = df.corr()
-    print(df_corr.head())
-    df_corr.to_csv('sp500corr.csv')
-    data1 = df_corr.values
-    fig1 = plt.figure()
-    ax1 = fig1.add_subplot(111)
-
-    heatmap1 = ax1.pcolor(data1, cmap=plt.cm.RdYlGn)
-    fig1.colorbar(heatmap1)
-
-    ax1.set_xticks(np.arange(data1.shape[1]) + 0.5, minor=False)
-    ax1.set_yticks(np.arange(data1.shape[0]) + 0.5, minor=False)
-    ax1.invert_yaxis()
-    ax1.xaxis.tick_top()
-    column_labels = df_corr.columns
-    row_labels = df_corr.index
-    ax1.set_xticklabels(column_labels)
-    ax1.set_yticklabels(row_labels)
-    plt.xticks(rotation=90)
-    heatmap1.set_clim(-1, 1)
-    plt.tight_layout()
-    plt.show()
+#def visualize_data():
+#    df = pd.read_csv('sp500_joined_closes.csv')
+#    df_corr = df.corr()
+#    print(df_corr.head())
+#    df_corr.to_csv('sp500corr.csv')
+#    data1 = df_corr.values
+#    fig1 = plt.figure()
+#    ax1 = fig1.add_subplot(111)
+#
+#    heatmap1 = ax1.pcolor(data1, cmap=plt.cm.RdYlGn)
+#    fig1.colorbar(heatmap1)
+#
+#    ax1.set_xticks(np.arange(data1.shape[1]) + 0.5, minor=False)
+#    ax1.set_yticks(np.arange(data1.shape[0]) + 0.5, minor=False)
+#    ax1.invert_yaxis()
+#    ax1.xaxis.tick_top()
+#    column_labels = df_corr.columns
+#    row_labels = df_corr.index
+#    ax1.set_xticklabels(column_labels)
+#    ax1.set_yticklabels(row_labels)
+#    plt.xticks(rotation=90)
+#    heatmap1.set_clim(-1, 1)
+#    plt.tight_layout()
+#    plt.show()
 
 
 def process_data_for_labels(ticker):
